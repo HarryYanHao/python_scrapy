c572de30d823c25e08acf556588bbb831679684d
Harry <harry@HarrydeMacBook-Pro.local>
2018-04-18 23:10:40 +0800

    first commit

---
+++ myProject/pipelines.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+import codecs
+
+
+class MyProjectPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('data.json', mode='wb', encoding='utf-8')#数据存储到data.json
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line.decode("unicode_escape"))
+
+        return item
---
+++ myProject/spiders/spider.py
@@ -0,0 +1,61 @@
+# -*-coding:utf-8-*-
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+# -*-coding:utf-8-*-
+import sys
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from myProject.items import MyprojectItem
+import re
+from w3lib.html import remove_tags
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+class ListSpider(CrawlSpider):
+     # 爬虫名称
+     name = "myProject"
+     # 设置下载延时
+     download_delay = 1
+     # 允许域名
+     allowed_domains = ["www.jj59.com"]
+     # 开始URL
+     start_urls = [
+         "https://www.jj59.com/jjart/422180"
+     ]
+     # 爬取规则,不带callback表示向该类url递归爬取
+     rules = (
+         #Rule(SgmlLinkExtractor(allow=(r'https://news.cnblogs.com/n/page/\d',))),
+         Rule(SgmlLinkExtractor(allow=(r'www.jj59.com/jjart/\d',)), callback='parse_content'),
+     )
+
+
+
+     # 解析内容函数
+     def parse_content(self, response):
+         def go_remove_tag(value):
+             # 移除标签
+             content = remove_tags(value)
+             # 移除空格 换行
+             return re.sub(r'[\t\r\n\s]', '', content)
+         item = MyprojectItem()
+
+         # 当前URL
+         title = response.selector.xpath('//*[contains(@class, "title")]/h1')[0].extract().decode('utf-8')
+         #img = response.selector.xpath('//div[@class="picurl"]/img/@src')[0].extract().decode('utf-8')
+
+         content = response.selector.xpath('/html/body/div[2]/div[1]/div[1]/div[4]')[0].extract().decode('utf-8')
+         #print(content);
+         #exit();
+         item['title'] = go_remove_tag(title)
+
+         #item['img'] = img
+         item['content'] = go_remove_tag(content)
+
+         yield item
+
+
+
